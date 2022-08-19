02f3be514112f86569b8a97134eb3ced61f56894
Sarge <sarge2033@gmail.com>
2022-08-19 19:33:04 +0300

    написал с циклом и натуральной степенью

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿    // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A
+    // в натуральную степень B
+
+    Console.Write("Введите A: ");
+    double A = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите B: ");
+    uint B = Convert.ToUInt32(Console.ReadLine());
+
+    // double x = Math.Pow(A, B);   //-  Мне так больше нравится.
+
+    double x = 1;
+
+    for(int i=1; i <= B; i++)
+    {
+        x = x * A;
+    }
+        
+    Console.Write("Ответ: " + x);
+
+
+
+
+
+
