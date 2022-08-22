c2fa23c7a0b9f4928c610bcec59afabf3001c5ea
Sarge <sarge2033@gmail.com>
2022-08-22 18:17:39 +0300

    задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(number);
+
+int Sum = 0;
+
+for(int i = 0; i < num.Length; i++)
+{
+       Sum = Sum + num[i] - '0';
+}
+Console.Write("Сумма: " + Sum);
